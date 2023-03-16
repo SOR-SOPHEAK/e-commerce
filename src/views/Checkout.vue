1df494de9ef4564c51c9a1e0d60b102dc894da3f
ssopheak2002@gmail.com <ssopheak2002@gmail.com>
2023-03-16 09:01:38 +0700

    Home page

---
+++ src/views/Checkout.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="checkout">
+      <h1>This is an about page</h1>
+
+      
+      <button>Cancel</button>
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .about {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+  
---
+++ src/views/Index.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+      <!-- <img alt="Vue logo" class="logo" src="@/assets/images/logo.svg" width="125" height="125" /> -->
+  
+      <div class="wrapper">
+        <nav>
+          <!-- klaeng del access tv link -->
+          <RouterLink to="/">Home</RouterLink> 
+          <RouterLink to="/about">About</RouterLink>
+          <RouterLink to="/product">Product</RouterLink>
+          <RouterLink to="/checkout">Checkout</RouterLink>
+          <button @click="$emit('checkout')">Go To Checkout</button>
+        </nav>
+      </div>
+    </header>
+  
+    <!-- klaeng render -->
+    <RouterView />
+  </template>
+  
